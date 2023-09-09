65e7393f51da74f43f899212364d0d98c00833dc
xwize <this.verb@gmail.com>
2023-09-09 17:31:57 +0100

    Fixed issue with redirect

--- prep.sh
+++ prep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "Enter Project Name: " prj
-awk -v P=$prj '{gsub(/CCGTemplate/,P); print}' conanfile.py > conanfile.py
-awk -v P=$prj '{gsub(/CCGTemplate/,P); print}' CMakeLists.txt > CMakeLists.txt
+awk -v P=$prj '{gsub(/CCGTemplate/,P); print}' conanfile.py | tee conanfile.py
+awk -v P=$prj '{gsub(/CCGTemplate/,P); print}' CMakeLists.txt | tee CMakeLists.txt
